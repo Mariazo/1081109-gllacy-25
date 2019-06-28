db5641704a74f4230ddec648a8942694288f7032
Maria <zmbereza@gmail.com>
2019-06-28 21:22:01 +0300

    подключепние скрипта

---
+++ js/script.js
@@ -0,0 +1,51 @@
+var link = document.querySelector(".callback-form-button");
+var popup = document.querySelector(".modal-feedback");
+var close = popup.querySelector(".modal-close");
+var form = popup.querySelector("form");
+var name = popup.querySelector("[name=feedback-name]");
+var email = popup.querySelector("[name=feedback-email]");
+
+var isStorageSupport = true;
+  var storage = "";
+
+  try {
+    storage = localStorage.getItem("email");
+  } catch (err) {
+    isStorageSupport = false;
+  }
+
+link.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  popup.classList.add("modal-show");
+  if (storage) {
+      login.value = storage;
+    } else {
+      email.focus();
+    }
+  name.focus();
+});
+
+close.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  popup.classList.remove("modal-show");
+});
+
+form.addEventListener("submit", function (evt) {
+    if (!login.value || !password.value) {
+      evt.preventDefault();
+      console.log("Нужно ввести имя и электронный адрес");
+    } else {
+      if (isStorageSupport) {
+      localStorage.setItem("email", email.value);
+    }
+    }
+  });
+
+  window.addEventListener("keydown", function (evt) {
+      if (evt.keyCode === 27) {
+        evt.preventDefault();
+        if (popup.classList.contains("modal-show")) {
+          popup.classList.remove("modal-show");
+        }
+      }
+    });
